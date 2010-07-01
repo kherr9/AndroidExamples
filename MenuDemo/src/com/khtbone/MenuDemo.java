fc41a41aed729ee7d54fbaddb4f6e974c5525d31
Kevin Herr <kherr9@gmail.com>
2010-07-01 10:38:57 -0700

    testing changes from browser

--- MenuDemo/src/com/khtbone/MenuDemo.java
+++ MenuDemo/src/com/khtbone/MenuDemo.java
@@ -9,5 +9,7 @@ public class MenuDemo extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        //this is a comment from Kevin online
     }
 }
