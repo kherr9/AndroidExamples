2f6ff0c80f046b91f396195df17794ca95bb5cd0
Kevin Herr <kherr9@gmail.com>
2010-07-02 15:20:14 -0500

    created project

---
+++ UiEvents01/src/com/khtbone/uievents01/UiEvents01.java
@@ -0,0 +1,13 @@
+package com.khtbone.uievents01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UiEvents01 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
