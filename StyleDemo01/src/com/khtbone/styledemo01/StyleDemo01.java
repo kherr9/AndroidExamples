211bca17aaa4603e9e42e11bad6b07ac932449a7
kevinherr <kevinherr@192.168.1.105>
2010-07-03 09:55:05 -0500

    Created black project

---
+++ StyleDemo01/src/com/khtbone/styledemo01/StyleDemo01.java
@@ -0,0 +1,13 @@
+package com.khtbone.styledemo01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StyleDemo01 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
