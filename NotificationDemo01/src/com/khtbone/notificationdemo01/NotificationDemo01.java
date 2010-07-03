767a548a7df98f74703211c5e42f1941ec3473d4
kevinherr <kevinherr@192.168.1.105>
2010-07-03 08:38:47 -0500

    adding project

---
+++ NotificationDemo01/src/com/khtbone/notificationdemo01/NotificationDemo01.java
@@ -0,0 +1,13 @@
+package com.khtbone.notificationdemo01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationDemo01 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
