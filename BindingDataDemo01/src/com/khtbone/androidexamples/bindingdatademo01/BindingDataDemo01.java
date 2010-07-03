cf6ec243ad419b1dffbf20fad9df46997ff77f64
kevinherr <kevinherr@192.168.1.105>
2010-07-03 14:50:45 -0500

    created blank project

---
+++ BindingDataDemo01/src/com/khtbone/androidexamples/bindingdatademo01/BindingDataDemo01.java
@@ -0,0 +1,13 @@
+package com.khtbone.androidexamples.bindingdatademo01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BindingDataDemo01 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
